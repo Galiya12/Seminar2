9988d01ccb7098fa9973df0571b352f8d120b0c2
Galiya12 <47217303+Galiya12@users.noreply.github.com>
2019-02-08 21:14:08 +0600

    Update try_except.py

--- try_except.py
+++ try_except.py
@@ -1,1 +1,37 @@
+class Error(Exception):
+   """Base class for other exceptions"""
+   pass
+
+class ValueTooSmallError(Error):
+   """Raised when the input value is too small"""
+   pass
+
+class ValueTooLargeError(Error):
+   """Raised when the input value is too large"""
+   pass
+
+class ValueIsZero(Error):
+   """Raised when the input value is zero"""
+   pass
+
+def guessNumber(guess):
+      number = 7
+      try:
+         if guess < number:
+            raise ValueTooSmallError
+         elif guess > number:
+            raise ValueTooLargeError
+         elif guess == 0:
+            raise ValueIsZero
+         elif guess == number:
+            print("Congratulations! You guessed it correctly.")
+      except ValueTooSmallError:
+         print("This value is smaller, try again!")
+         print()
+      except ValueTooLargeError:
+         print("This value is larger, try again!")
+         print()
+      except ValueIsZero:
+         print("Desired value is not zero!")
+
 
